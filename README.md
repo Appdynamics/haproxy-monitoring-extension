69f4059fe35951926a3fae8198a4b033f6273919
Balakrishna V <balakrishna.v@imaginea.com>
2014-01-17 14:29:34 +0530

    Added files and updated README

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-haproxy-monitoring-extension
-============================
+# AppDynamics HAProxy Monitoring Extension
+
+This extension works only with the standalone machine agent.
+
+##Use Case
+
+HAProxy is an open source, high performance TCP/HTTP Load Balancer commonly used to improve the performance of web sites and services by spreading requests across multiple servers. 
+The HAProxy Monitoring extension collects key metrics from HAProxy Load balancer of the underlying proxies/servers and presents them to the AppDynamics Metric Browser. 
+
+
+##Installation
+
+1. Run 'ant package' from the haproxy-monitoring-extension directory
+2. Download the file HAProxyMonitor.zip located in the 'dist' directory into \<machineagent install dir\>/monitors/
+3. Unzip the downloaded file
+4. In \<machineagent install dir\>/monitors/HAProxyMonitor/, open monitor.xml and configure the HAProxy parameters.
+     <pre>
+     URI of the haproxy CSV stats. See the 'CSV Export' link on your haproxy stats page
+     &lt;argument name="url" is-required="true" default-value="http://demo.1wt.eu/;csv" /&gt;
+     &lt;argument name="username" is-required="false" default-value="" /&gt;
+     &lt;argument name="password" is-required="false" default-value="" /&gt;
+     proxy names you wish to monitor as a comma separated values. If empty all the proxies are monitored.
+     &lt;argument name="proxynames" is-required="false" default-value="" /&gt;
+     </pre>
+5. Restart the Machine Agent. 
+ 
+In the AppDynamics Metric Browser, look for: Application Infrastructure Performance  | \<Tier\> | Custom Metrics | HAProxy
+
+##Directory Structure
+
+<table><tbody>
+<tr>
+<th align="left"> File/Folder </th>
+<th align="left"> Description </th>
+</tr>
+<tr>
+<td class='confluenceTd'> conf </td>
+<td class='confluenceTd'> Contains the monitor.xml </td>
+</tr>
+<tr>
+<td class='confluenceTd'> lib </td>
+<td class='confluenceTd'> Contains third-party project references </td>
+</tr>
+<tr>
+<td class='confluenceTd'> src </td>
+<td class='confluenceTd'> Contains source code to the HAProxy Monitoring Extension </td>
+</tr>
+<tr>
+<td class='confluenceTd'> dist </td>
+<td class='confluenceTd'> Only obtained when using ant. Run 'ant build' to get binaries. Run 'ant package' to get the distributable .zip file </td>
+</tr>
+<tr>
+<td class='confluenceTd'> build.xml </td>
+<td class='confluenceTd'> Ant build script to package the project (required only if changing Java code) </td>
+</tr>
+</tbody>
+</table>
+
+## Metrics
+
+| Metric Name | Description |
+|----------------|-------------|
+|status				| 1 (UP/OPEN) 0 (DOWN)|
+|qcur				| current queued requests|
+|scur				| current sessions|
+|stot				| total sessions|
+|bin				| Bytes In	|
+|bout				| Bytes Out|
+|ereq				| error requests|
+|eresp				| response errors|
+|econ				| connection errors|
+|act				| server is active (server), number of active servers (backend)|
+|bck				| server is backup (server), number of backup servers (backend)|
+
+## Custom Dashboard
+![]()
+
+##Contributing
+
+Always feel free to fork and contribute any changes directly here on GitHub.
+
+##Community
+
+Find out more in the [AppSphere]() community.
+
+##Support
+
+For any questions or feature request, please contact [AppDynamics Center of Excellence](mailto:ace-request@appdynamics.com).
+
+
--- src/main/java/com/appdynamics/monitors/haproxy/HAProxyMonitor.java
+++ src/main/java/com/appdynamics/monitors/haproxy/HAProxyMonitor.java
@@ -1,18 +1,18 @@
 /**
-* Copyright 2013 AppDynamics, Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2013 AppDynamics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.appdynamics.monitors.haproxy;
 
@@ -52,18 +52,18 @@ import com.singularity.ee.util.httpclient.IHttpClientWrapper;
 import com.singularity.ee.util.log4j.Log4JLogger;
 
 public class HAProxyMonitor extends AManagedMonitor {
-	
+
 	private static Logger logger = Logger.getLogger(HAProxyMonitor.class.getName());
 	private static final String metricPathPrefix = "Custom Metrics|HAProxy|";
-	
+
 	private WritableWorkbook workbook;
-	
+
 	private Map<String, Integer> dictionary;
-	
+
 	private String responseString;
-	
-	private List<String> proxiesToBeMonitored = new ArrayList<String>();
-	
+
+	private List<String> proxiesListedInConfigFile = new ArrayList<String>();
+
 	public HAProxyMonitor() {
 		dictionary = new HashMap<String, Integer>();
 		dictionary.put("# pxname", 0);
@@ -80,87 +80,93 @@ public class HAProxyMonitor extends AManagedMonitor {
 		dictionary.put("act", 19);
 		dictionary.put("bck", 20);
 	}
-	
+
 	/*
 	 * Main execution method that uploads the metrics to AppDynamics Controller
-	 * @see com.singularity.ee.agent.systemagent.api.ITask#execute(java.util.Map, com.singularity.ee.agent.systemagent.api.TaskExecutionContext)
+	 * 
+	 * @see
+	 * com.singularity.ee.agent.systemagent.api.ITask#execute(java.util.Map,
+	 * com.singularity.ee.agent.systemagent.api.TaskExecutionContext)
 	 */
-	public TaskOutput execute(Map<String, String> taskArguments,
-			TaskExecutionContext arg1) throws TaskExecutionException {
-		
+	public TaskOutput execute(Map<String, String> taskArguments, TaskExecutionContext arg1) throws TaskExecutionException {
 		try {
 			// connect to url with the arguments provided and get response string
 			connect(taskArguments);
-            // reads the csv  output and writes the response to a spreadsheet which is used inturn to get the metrics
-            writeResponseToWorkbook(responseString);
-            
-            Map<Integer, String> proxies = getAllProxyAndTypes(dictionary.get("# pxname"));
-            Map<Integer, String> proxyTypes = getAllProxyAndTypes(dictionary.get("svname"));
-            
-            if(proxiesToBeMonitored.size() != 0) {
-            	for(Iterator<Map.Entry<Integer, String>> it=proxies.entrySet().iterator();it.hasNext();) {
-            		Map.Entry<Integer, String> entry = it.next();
-            		if(!proxiesToBeMonitored.contains(entry.getValue())) {
-            			it.remove();
-            		}
-            	}
-            }
-            // Prints metrics to Controller Metric Browser
-            for(Map.Entry<Integer, String> proxy : proxies.entrySet()) {
-            	printMetric(getMetricPrefix()+ proxy.getValue() + "|" + proxyTypes.get(proxy.getKey()) + "|","status" , getStatus(proxy.getKey()), MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
-            	
-            	printMetric(proxy, proxyTypes, "qcur", "queued_requests");
-            	printMetric(proxy, proxyTypes, "scur", "current sessions");
-            	printMetric(proxy, proxyTypes, "stot", "total sessions");
-            	printMetric(proxy, proxyTypes, "bin", "bytes in");
-            	printMetric(proxy, proxyTypes, "bout", "bytes out");
-            	printMetric(proxy, proxyTypes, "ereq", "error requests");
-            	printMetric(proxy, proxyTypes, "econ", "connection errors");
-            	printMetric(proxy, proxyTypes, "eresp", "response errors");
-            	printMetric(proxy, proxyTypes, "act", "active servers");
-            	printMetric(proxy, proxyTypes, "bck", "backup servers");
-            }
-            
-            return new TaskOutput("ActiveMQ Metric Upload Complete");
+			// reads the csv output and writes the response to a spreadsheet
+			// which is used inturn to get the metrics
+			writeResponseToWorkbook(responseString);
+
+			Map<Integer, String> proxiesToBeMonitored = getAllProxyAndTypes(dictionary.get("# pxname"));
+			Map<Integer, String> proxyTypes = getAllProxyAndTypes(dictionary.get("svname"));
+
+			if (proxiesListedInConfigFile.size() != 0) {
+				for (Iterator<Map.Entry<Integer, String>> it = proxiesToBeMonitored.entrySet().iterator(); it.hasNext();) {
+					Map.Entry<Integer, String> entry = it.next();
+					if (!proxiesListedInConfigFile.contains(entry.getValue())) {
+						it.remove();
+					}
+				}
+			}
+			// Prints metrics to Controller Metric Browser
+			for (Map.Entry<Integer, String> proxy : proxiesToBeMonitored.entrySet()) {
+				printMetric(getMetricPrefix() + proxy.getValue() + "|" + proxyTypes.get(proxy.getKey()) + "|", "status", getStatus(proxy.getKey()),
+						MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+						MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
+
+				printMetric(proxy, proxyTypes, "qcur", "queued_requests");
+				printMetric(proxy, proxyTypes, "scur", "current sessions");
+				printMetric(proxy, proxyTypes, "stot", "total sessions");
+				printMetric(proxy, proxyTypes, "bin", "bytes in");
+				printMetric(proxy, proxyTypes, "bout", "bytes out");
+				printMetric(proxy, proxyTypes, "ereq", "error requests");
+				printMetric(proxy, proxyTypes, "econ", "connection errors");
+				printMetric(proxy, proxyTypes, "eresp", "response errors");
+				printMetric(proxy, proxyTypes, "act", "active servers");
+				printMetric(proxy, proxyTypes, "bck", "backup servers");
+			}
+
+			return new TaskOutput("ActiveMQ Metric Upload Complete");
 		} catch (Exception e) {
 			logger.error("ActiveMQ Metric upload failed");
-            return new TaskOutput("ActiveMQ Metric upload failed");
+			return new TaskOutput("ActiveMQ Metric upload failed");
 		}
 	}
+
 	/**
 	 * Validates the task arguments passed and connects to HAProxy CSV page.
+	 * 
 	 * @param taskArguments
 	 */
 	private void connect(Map<String, String> taskArguments) {
-        if(!taskArguments.containsKey("url") || !taskArguments.containsKey("username") || !taskArguments.containsKey("password"))
-        {
-            logger.error("Monitor.xml needs to contain all required task arguments");
-            throw new RuntimeException("Monitor.xml needs to contain all required task arguments");
-        }
-        String url = taskArguments.get("url");
-        URL aurl;
+		if (!taskArguments.containsKey("url") || !taskArguments.containsKey("username") || !taskArguments.containsKey("password")) {
+			logger.error("Monitor.xml needs to contain all required task arguments");
+			throw new RuntimeException("Monitor.xml needs to contain all required task arguments");
+		}
+		String url = taskArguments.get("url");
+		URL aurl;
 		try {
 			aurl = new URL(url);
 			String host = aurl.getHost();
-	        String userName = taskArguments.get("username");
-	        String password = taskArguments.get("password");
-	        
-	        if(url != null && url !="") {
-	        	responseString = getResponseString(url, host, userName, password);
-	        }
-	        logger.error("Connected to " + url);
+			String userName = taskArguments.get("username");
+			String password = taskArguments.get("password");
+
+			if (url != null && url != "") {
+				responseString = getResponseString(url, host, userName, password);
+			}
+			logger.error("Connected to " + url);
 		} catch (MalformedURLException e) {
 			logger.error("URL null or empty in monitor.xml");
-            throw new RuntimeException("URL null or empty in monitor.xml");
+			throw new RuntimeException("URL null or empty in monitor.xml");
 		}
-        
-        if(taskArguments.containsKey("proxynames") &&
-                null != taskArguments.get("proxynames") && !taskArguments.get("proxynames").equals("")) {
-        	proxiesToBeMonitored = Arrays.asList(taskArguments.get("proxynames").split(","));
-        }
+
+		if (taskArguments.containsKey("proxynames") && null != taskArguments.get("proxynames") && !taskArguments.get("proxynames").equals("")) {
+			proxiesListedInConfigFile = Arrays.asList(taskArguments.get("proxynames").split(","));
+		}
 	}
+
 	/**
 	 * Returns http response as a csv string
+	 * 
 	 * @param url
 	 * @param host
 	 * @param username
@@ -172,12 +178,14 @@ public class HAProxyMonitor extends AManagedMonitor {
 		HttpExecutionRequest request = new HttpExecutionRequest(url, "", HttpOperation.GET);
 		httpClient.authenticateHost(host, 80, "", username, password, true);
 		HttpExecutionResponse response = httpClient.executeHttpOperation(request, new Log4JLogger(logger));
-		
+
 		responseString = response.getResponseBody();
 		return responseString;
 	}
+
 	/**
 	 * Writes the csv respone to a spreadsheet as a matrix of proxies Vs stats
+	 * 
 	 * @param responseString
 	 * @throws Exception
 	 */
@@ -186,19 +194,17 @@ public class HAProxyMonitor extends AManagedMonitor {
 			OutputStream outputStream = new ByteArrayOutputStream();
 			outputStream.write(responseString.getBytes());
 			workbook = Workbook.createWorkbook(outputStream);
-			
+
 			WritableSheet sheet = workbook.createSheet("First Sheet", 0);
-			
+
 			BufferedReader reader = new BufferedReader(new StringReader(responseString));
 			String line;
 			int j = 0;
-			while((line = reader.readLine()) != null)
-			{
+			while ((line = reader.readLine()) != null) {
 				Pattern p = Pattern.compile(",");
-				String [] result = p.split(line);
-				for(int i = 0; i < result.length; i++)
-				{
-					Label label = new jxl.write.Label(i,j,result[i]);
+				String[] result = p.split(line);
+				for (int i = 0; i < result.length; i++) {
+					Label label = new jxl.write.Label(i, j, result[i]);
 					sheet.addCell(label);
 				}
 				j++;
@@ -210,65 +216,76 @@ public class HAProxyMonitor extends AManagedMonitor {
 			throw new RuntimeException("Error while writing response to workbook stream");
 		}
 	}
+
 	/**
-	 * Returns all proxies and proxy types as a Map of row index and Proxy Name. Column Index is 0 for Px Name and 1 for Server Name
+	 * Returns all proxies and proxy types as a Map of row index and Proxy Name.
+	 * Column Index is 0 for Px Name and 1 for Server Name
+	 * 
 	 * @param columnIndex
 	 * @return
 	 */
 	private Map<Integer, String> getAllProxyAndTypes(int columnIndex) {
 		Map<Integer, String> proxiesMap = new HashMap<Integer, String>();
 		int rows = getSheet().getRows();
-		for(int i=1; i<rows; i++) {
+		for (int i = 1; i < rows; i++) {
 			proxiesMap.put(i, getSheet().getCell(columnIndex, i).getContents());
 		}
 		return proxiesMap;
 	}
+
 	/**
 	 * Prints the metric to the AppDynamics Controller Metric Browser
+	 * 
 	 * @param proxy
 	 * @param proxyTypes
 	 * @param metricKey
 	 * @param metricName
 	 */
-	private void printMetric(Entry<Integer, String> proxy,
-			Map<Integer, String> proxyTypes, String metricKey, String metricName) {
-		if(!getCellContents(dictionary.get(metricKey), proxy.getKey()).equals("")) {
-    		printMetric(getMetricPrefix()+ proxy.getValue() + "|" + proxyTypes.get(proxy.getKey()) + "|", metricName, getCellContents(dictionary.get(metricKey), proxy.getKey()), MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
-    	}
+	private void printMetric(Entry<Integer, String> proxy, Map<Integer, String> proxyTypes, String metricKey, String metricName) {
+		if (!getCellContents(dictionary.get(metricKey), proxy.getKey()).equals("")) {
+			printMetric(getMetricPrefix() + proxy.getValue() + "|" + proxyTypes.get(proxy.getKey()) + "|", metricName,
+					getCellContents(dictionary.get(metricKey), proxy.getKey()), MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE,
+					MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
+		}
 	}
-	
-	private void printMetric(String metricPath, String metricName, Object metricValue, String aggregation, String timeRollup, String cluster)
-    {
-        MetricWriter metricWriter = super.getMetricWriter(metricPath + metricName, aggregation, timeRollup, cluster);
-            metricWriter.printMetric(String.valueOf(metricValue));
-    }
-	
-	private String getMetricPrefix()
-    {
-        return metricPathPrefix;
-    }
-	
+
+	private void printMetric(String metricPath, String metricName, Object metricValue, String aggregation, String timeRollup, String cluster) {
+		MetricWriter metricWriter = super.getMetricWriter(metricPath + metricName, aggregation, timeRollup, cluster);
+		metricWriter.printMetric(String.valueOf(metricValue));
+	}
+
+	private String getMetricPrefix() {
+		return metricPathPrefix;
+	}
+
 	/**
-	 * Gets the status of the proxy/server. If it is Up | Open, status is set to 1; if not to 0
+	 * Gets the status of the proxy/server. If it is Up | Open, status is set to
+	 * 1; if not to 0
+	 * 
 	 * @param proxyRowIndex
 	 * @return
 	 */
 	private String getStatus(int proxyRowIndex) {
 		String status = "0";
-		if(getCellContents(dictionary.get("status"), proxyRowIndex).equals("UP") || getCellContents(dictionary.get("status"), proxyRowIndex).equals("OPEN")) {
+		if (getCellContents(dictionary.get("status"), proxyRowIndex).equals("UP")
+				|| getCellContents(dictionary.get("status"), proxyRowIndex).equals("OPEN")) {
 			status = "1";
 		}
 		return status;
 	}
+
 	/**
 	 * Returns the zeroth sheet of the workbook
+	 * 
 	 * @return
 	 */
 	private Sheet getSheet() {
 		return workbook.getSheet(0);
 	}
+
 	/**
 	 * Gets the contents of the cell in the workbook given column and row
+	 * 
 	 * @param column
 	 * @param row
 	 * @return String
@@ -276,5 +293,14 @@ public class HAProxyMonitor extends AManagedMonitor {
 	private String getCellContents(int column, int row) {
 		String contents = getSheet().getCell(column, row).getContents();
 		return contents;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		HAProxyMonitor monitor = new HAProxyMonitor();
+
+		String responseString = monitor.getResponseString("http://demo.1wt.eu/;csv", "demo.1wt.eu", "", "");
+
+		System.out.println(responseString);
 	}
 }
