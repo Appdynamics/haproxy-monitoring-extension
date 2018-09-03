e42bc62691ba9f6077c218c7ff8bc8d0816683d5
Prashant Mehta <prashant.mehta@appdynamics.com>
2018-09-03 17:32:37 +0530

    Making proxy-server selection configurable and regex supported

--- src/main/java/com/appdynamics/extensions/haproxy/config/ProxyServerConfig.java
+++ src/main/java/com/appdynamics/extensions/haproxy/config/ProxyServerConfig.java
@@ -1,35 +1,8 @@
 package com.appdynamics.extensions.haproxy.config;
 
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-
-@XmlAccessorType(XmlAccessType.FIELD)
 public class ProxyServerConfig {
-    @XmlAttribute
-    private String name;
-    @XmlAttribute
-    public String children;
-    @XmlElement(name = "servers")
     private ServerConfig[] serverConfigs;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setChildren(String children) {
-        this.children = children;
-    }
-
-    public String getChildren() {
-        return children;
-    }
 
     public void setServerConfigs(ServerConfig[] serverConfigs) {
         this.serverConfigs = serverConfigs;
@@ -39,4 +12,3 @@ public class ProxyServerConfig {
         return serverConfigs;
     }
 }
-
